58fb3a577849cf1eebd8c508e31688a4ad899822
sonicgg@gmail.com <sonicgg@gmail.com@71ec348d-6686-a967-ad19-0c62fc11cc81>
2011-04-06 15:12:51 +0000

    a

---
+++ trunk/app_identity_java/src/com/google/demo/appidentity/Access.java
@@ -0,0 +1,98 @@
+package com.google.demo.appidentity;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+import net.oauth.jsontoken.AudienceChecker;
+import net.oauth.jsontoken.JsonTokenParser;
+import net.oauth.jsontoken.crypto.SignatureAlgorithm;
+import net.oauth.jsontoken.discovery.UrlBasedVerifierProvider;
+import net.oauth.jsontoken.discovery.VerifierProviders;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.security.SignatureException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Access extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    resp.setContentType("text/html");
+    resp.getWriter().println("<html><head>");
+    resp.getWriter().println("<title>App Identity API Demo</title>");
+    resp.getWriter().println("</head><body>");
+
+    resp.getWriter().println("<div id=createbox><form action=/access method=post>"
+        + "Input your resource page URL:<input type=text name=resource_url value = \"http://app-identity-java.appspot.com/resource\" size=100 />" + ""
+        + "<input type=submit name=access value=\"Access\">" + "");
+
+    String urlString = req.getParameter("resource_url");
+    
+    if (urlString != null) {
+      URL url;
+      urlString += "?jwt=";
+      try {
+        urlString += App_identity_javaServlet.createJsonToken();
+      } catch (SignatureException e) {
+        e.printStackTrace();
+      }
+      urlString += "&certurl=";
+      urlString += URLEncoder.encode("http://app-identity-java.appspot.com/certs");
+      resp.getWriter().println("<p>" + "<b>Send request to: </b>" + urlString + "</p>");
+      
+      try {
+        url = new URL(urlString);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+          InputStreamReader in = new InputStreamReader((InputStream) connection.getContent());
+          BufferedReader buff = new BufferedReader(in);
+          StringBuffer content = new StringBuffer();
+          String line = "";
+          do {
+            content.append(line + "\n");
+            line = buff.readLine();
+          } while (line != null);
+          
+          resp.getWriter().println("<p>" + "<b>Result: </b>" + content.toString() + "</p>");
+        } else if (connection.getResponseCode() == HttpURLConnection.HTTP_FORBIDDEN) {
+          resp.getWriter().println(
+              "<p>" + "<b>Access denied </b>" + Resource.getAccessInfo() + "</p>");
+        } else {
+          resp.getWriter().println("<b><p>" + "Unknown error </b>" + "</p>");
+        }
+      } catch (MalformedURLException e) {
+        resp.getWriter().println("<p> <b>" + "Invalid URl </b>" + "</p>");
+        return;
+      }
+    }
+    
+    resp.getWriter().println(
+        "<p> <a href=\"" 
+        + "/" 
+        + "\">"
+        + "Back to main page" 
+        + "</a></p>");
+
+    resp.getWriter().println("</body>");
+    resp.getWriter().println("</html>");
+  }
+  
+  @Override
+  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    doGet(req, resp);
+  }
+}
--- trunk/app_identity_java/src/com/google/demo/appidentity/Resource.java
+++ trunk/app_identity_java/src/com/google/demo/appidentity/Resource.java
@@ -1,34 +1,20 @@
 package com.google.demo.appidentity;
 
-import com.google.common.collect.Lists;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 
 import net.oauth.jsontoken.AudienceChecker;
 import net.oauth.jsontoken.JsonTokenParser;
-import net.oauth.jsontoken.crypto.RsaSHA256Verifier;
 import net.oauth.jsontoken.crypto.SignatureAlgorithm;
-import net.oauth.jsontoken.crypto.Verifier;
-import net.oauth.jsontoken.discovery.VerifierProvider;
+import net.oauth.jsontoken.discovery.UrlBasedVerifierProvider;
 import net.oauth.jsontoken.discovery.VerifierProviders;
 
-import org.apache.commons.codec.binary.Base64;
-
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.security.SignatureException;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -38,93 +24,60 @@ public class Resource extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     String jwt = req.getParameter("jwt");
-    String publicCertUrl = App_identity_javaServlet.getPublicCertUrl();
-    boolean verified = verify(jwt, publicCertUrl);
+    String certUrl = req.getParameter("certurl");
+    boolean verified = verify(jwt, certUrl);
 
-    resp.setContentType("text/plain");
+    resp.setContentType("text/html");
+    resp.getWriter().println("<html><head>");
+    resp.getWriter().println("<title>App Identity API Demo</title>");
+    resp.getWriter().println("</head><body>");
+    
     if (verified) {
-      resp.getWriter().println("Access OK");
+      resp.getWriter().println("Access Succeed, http://app-identity-java.appspot.com/resource confirmed I am <b>" + certUrl + "</b>");
     } else {
+      resp.getWriter().println("Authentication fails");
+      
+      resp.getWriter().println(
+          "<p> <a href=\"" 
+          + "http://app-identity-java.appspot.com/access"
+          + "\">"
+          + "Access this page from http://app-identity-java.appspot.com/access"
+          + "</a></p>");
+      
+      resp.getWriter().println(
+          "<p> <a href=\"" 
+          + "http://app-identity-python.appspot.com/access"
+          + "\">"
+          + "Access this page from http://app-identity-python.appspot.com/access"
+          + "</a></p>");
+      
       resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
     }
+    
+    resp.getWriter().println("</body>");
+    resp.getWriter().println("</html>");
   }
 
   public boolean verify(String jwt, String publicCertUrl) {
-    VerifierProviders providers = new VerifierProviders();
-    providers.setVerifierProvider(SignatureAlgorithm.RS256, new myKeyLocator(publicCertUrl));
-    JsonTokenParser parser = new JsonTokenParser(providers, new MyAudienceChecker());
+    if (publicCertUrl == null || publicCertUrl.isEmpty()) {
+      return false;
+    }
     if (jwt == null || jwt.isEmpty()) {
       return false;
     }
+
+    VerifierProviders providers = new VerifierProviders();
+    providers.setVerifierProvider(
+        SignatureAlgorithm.RS256, new UrlBasedVerifierProvider(publicCertUrl));
+    JsonTokenParser parser = new JsonTokenParser(providers, new MyAudienceChecker());
 
     try {
       parser.verifyAndDeserialize(jwt);
     } catch (SignatureException e) {
       return false;
     }
+    storeAccessInfo(publicCertUrl);
     return true;
-  }
-
-  static class myKeyLocator implements VerifierProvider {
-
-    private final String publicCertUrl;
-
-    public myKeyLocator(String publicCertUrl) {
-      this.publicCertUrl = publicCertUrl;
-    }
-
-    @Override
-    public List<Verifier> findVerifier(String issuer, String keyId) {
-      try {
-        URL url = new URL(publicCertUrl);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-          
-          InputStreamReader in = new InputStreamReader((InputStream) connection.getContent());
-          BufferedReader buff = new BufferedReader(in);
-          StringBuffer content = new StringBuffer();
-          String line = "";
-          do {
-            line = buff.readLine();
-            content.append(line + "\n");
-          } while (line != null);
-          
-          JsonParser parser = new JsonParser();
-          JsonObject jsonObject = parser.parse(content.toString()).getAsJsonObject();
-          List<Verifier> verifiers = Lists.newArrayList();
-          
-          for (Map.Entry<String, JsonElement> cert : jsonObject.entrySet()) {
-            String x509PemCertString = cert.getValue().getAsString();
-            // Parse pem format
-            String[] parts = x509PemCertString.split("\n");
-            if (parts.length < 3) {
-              System.err.println("bad cert: " + x509PemCertString);
-              return null;
-            }
-            String x509CertString = "";
-            for (int i = 1; i < parts.length - 1; i++) {
-              x509CertString += parts[i];
-            }
-            // parse x509
-            byte[] certBytes = Base64.decodeBase64(x509CertString);
-            CertificateFactory factory = CertificateFactory.getInstance("X509");
-            X509Certificate x509Cert = 
-              (X509Certificate) factory.generateCertificate(new ByteArrayInputStream(certBytes));
-            verifiers.add(new RsaSHA256Verifier(x509Cert.getPublicKey()));
-          }
-          return verifiers;  
-        } else {
-          return null;
-        }
-      } catch (MalformedURLException e) {
-        return null;
-      } catch (IOException e) {
-        return null;
-      } catch (CertificateException e) {
-        return null;
-      }
-    }
   }
 
   static class MyAudienceChecker implements AudienceChecker {
@@ -132,5 +85,27 @@ public class Resource extends HttpServlet {
     public void checkAudience(String arg0) throws SignatureException {
       // pass
     }
+  }
+
+  static public String getAccessInfo() {
+    Key certKey = KeyFactory.createKey("LastAccess", "LastAccess");
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    try {
+      Entity entity = datastore.get(certKey);
+      if (entity != null) {
+        return (String) entity.getProperty("name");
+      } else {
+        return "";
+      }
+    } catch (EntityNotFoundException e) {
+      return "";
+    }
+  }
+
+  static public void storeAccessInfo(String certUrl) {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Entity entity = new Entity("LastAccess", "LastAccess");
+    entity.setProperty("name", certUrl);
+    datastore.put(entity);
   }
 }
