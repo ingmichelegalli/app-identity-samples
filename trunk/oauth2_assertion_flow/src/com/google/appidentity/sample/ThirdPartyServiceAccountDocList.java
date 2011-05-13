d3a704e3908d6b514677b2343fdf02513afec02b
sonicgg@gmail.com <sonicgg@gmail.com@71ec348d-6686-a967-ad19-0c62fc11cc81>
2011-05-13 18:00:44 +0000

    1

--- trunk/oauth2_assertion_flow/src/com/google/appidentity/sample/ThirdPartyServiceAccountDocList.java
+++ trunk/oauth2_assertion_flow/src/com/google/appidentity/sample/ThirdPartyServiceAccountDocList.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ThirdPartyServiceAccountDocList extends HttpServlet {
   
+  // If you interested in this flow, send email to jcai@google.com for registration.
+  // We will eventually provide UI for registration external service account.
   public static final String EXTERNAL_ACCOUNT_NAME = "robot@testing.gserviceaccount.com";
     
   public ThirdPartyServiceAccountDocList() {
